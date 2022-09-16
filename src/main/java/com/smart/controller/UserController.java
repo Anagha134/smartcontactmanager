f3081fa6373521c83337cb30ac42bf9b4f61512b
Anagha More <moreanagha13@gmail.com>
2022-09-16 22:55:56 +0530

    showing the contacts of users in effective way

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.security.Principal;
+import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.smart.dao.ContactRepository;
 import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
 import com.smart.entities.User;
@@ -31,6 +33,9 @@ public class UserController {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private ContactRepository contactRepository;
 	
 	//method for adding common data to response
 	@ModelAttribute
@@ -131,6 +136,27 @@ public class UserController {
 		}
 		
 		return "normal/add_contact_form";
+	}
+	
+	//show contacts handlers
+	@GetMapping("/show-contacts")
+	public String showContacts(Model m,Principal principal) {
+		//want to display the list of contacts from database
+		m.addAttribute("title","Show User Contacts");
+		//first find the which user is login with help of principal
+		String userName = principal.getName();
+	
+		User user = this.userRepository.getUserByUserName(userName);
+		
+		//and then display all contacts of that user's from database
+		List<Contact> contacts = this.contactRepository.findContactsByUser(user.getId());
+		
+		
+		m.addAttribute("contacts",contacts);
+	
+		
+		
+		return "normal/show_contacts";
 	}
 	
 	

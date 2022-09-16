44f6d4e2b57f2f57d07dbeedc87ddcd85938f249
Anagha More <moreanagha13@gmail.com>
2022-09-16 17:24:35 +0530

    worked with all backend of add contact page in userdashboard

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -1,17 +1,29 @@
 package com.smart.controller;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
 import com.smart.entities.User;
+import com.smart.helper.Message;
 
 @Controller
 @RequestMapping("/user") //created a user handler url pattern
@@ -45,7 +57,7 @@ public class UserController {
 	}
 	
 	//open add from handler
-	@GetMapping("add-contact")
+    @GetMapping("/add-contact")
 	public String openAddcontactform(Model model) {
 		
 		model.addAttribute("title", "Add Contact");
@@ -54,6 +66,72 @@ public class UserController {
 		return "normal/add_contact_form";
 	}
 	
+	
+	//processing add contact form 
+	@PostMapping("/process-contact")
+	//all the data will be store in contact obj when the feilds will be matched in form's and contact class variables
+	public String processContact(@ModelAttribute Contact contact,@RequestParam("profileImage") MultipartFile file,Principal principal, HttpSession session) {
+		
+		//use to remove the details principal
+		try {
+		String name = principal.getName();
+		User user = this.userRepository.getUserByUserName(name);
+		
+		//processing and uplaoding file
+		
+		if(file.isEmpty()) {
+			//if file is empty then try our message
+			System.out.println("File is empty");
+			
+			
+			
+		}
+		else {
+			//upload the file to folder and update filename to contact details 
+			contact.setImage(file.getOriginalFilename());
+			
+			//to find the path of file
+			File savefile = new ClassPathResource("static/img").getFile();
+			
+			Path path = Paths.get(savefile.getAbsolutePath()+File.separator+file.getOriginalFilename());
+			
+			//
+			Files.copy(file.getInputStream(),path ,StandardCopyOption.REPLACE_EXISTING);
+			System.out.println("Image is uploaded");
+		}
+		//contact ko user dena hai 
+		contact.setUser(user);
+		
+		//adding the contact in user  list
+		user.getContacts().add(contact);
+		
+		//value is save in database
+		this.userRepository.save(user);
+		
+		
+		System.out.println("DATA "+ contact);
+		
+		System.out.println("Added to data base ");
+		
+		
+		//message success.........................
+		session.setAttribute("message", new Message("Your Contact is added!!!","success"));
+		
+		
+		
+		}catch(Exception e) {
+			System.out.println("ERROR "+e.getMessage());
+			e.printStackTrace();
+			
+			//message error..................
+			session.setAttribute("message", new Message("Something went Wrong, Try Again!!!","danger"));
+			
+			
+			
+		}
+		
+		return "normal/add_contact_form";
+	}
 	
 	
 }

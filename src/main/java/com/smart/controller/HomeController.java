61aaa7fbdb81cd37e0cbb0d839f97e31720c0b3a
Anagha More <moreanagha13@gmail.com>
2022-09-16 11:08:42 +0530

    adding sidebar

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 //import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -100,4 +101,20 @@ public class HomeController {
 		}
 		
 	}
+	
+	//handler for custom login
+	@GetMapping("/signin")
+	public String customLogin(Model model) {
+		
+		model.addAttribute("title","Login Page");
+		return "login";
+	}
+	
+	
+	
+	
+	
+	
+	
 }
+
